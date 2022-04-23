cb0b5bb0db397a943762f950c876d549138c2672
wlt <1319546531@qq.com>
2022-04-23 14:48:34 +0800

    完成录入的封装，查询未做完

---
+++ MainPage.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+import tkinter as tk
+from view import AboutFrame,ModifyFrame,InsertFrame,SearchFrame,DeleteFrame,SortFrame,TotalFrame,ContentFrame
+
+class MainPage:
+    def __init__(self,master:tk.Tk):
+        self.root = master
+        self.root.title('学生信息管理系统')
+        self.root.geometry('600x400')
+        self.create_page()
+
+    def create_page(self):
+        self.about_frame = AboutFrame(self.root)
+        self.modify_frame = ModifyFrame(self.root)
+        self.insert_frame = InsertFrame(self.root)
+        self.search_frame = SearchFrame(self.root)
+        self.delete_frame = DeleteFrame(self.root)
+        self.sort_frame = SortFrame(self.root)
+        self.total_frame = TotalFrame(self.root)
+        self.content_frame = ContentFrame(self.root)
+
+
+        menubar = tk.Menu(self.root)
+        menubar.add_command(label='录入',command=self.show_insert)
+        menubar.add_command(label='查询',command=self.show_search)
+        menubar.add_command(label='删除',command=self.show_delete)
+        menubar.add_command(label='修改',command=self.show_modify)
+        menubar.add_command(label='排序',command=self.show_sort)
+        menubar.add_command(label='统计',command=self.show_total)
+        menubar.add_command(label='概览',command=self.show_content)
+        menubar.add_command(label='关于',command=self.show_about)
+        self.root['menu'] = menubar
+
+    def show_about(self):
+        self.about_frame.pack()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack_forget()
+        self.content_frame.pack_forget()
+
+    def show_insert(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack_forget()
+        self.content_frame.pack_forget()
+        pass
+
+    def show_search(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack_forget()
+        self.content_frame.pack_forget()
+        pass
+
+    def show_delete(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack_forget()
+        self.content_frame.pack_forget()
+        pass
+
+    def show_modify(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack_forget()
+        self.content_frame.pack_forget()
+        pass
+
+    def show_sort(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack()
+        self.total_frame.pack_forget()
+        self.content_frame.pack_forget()
+        pass
+
+    def show_total(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack()
+        self.content_frame.pack_forget()
+        pass
+
+    def show_content(self):
+        self.about_frame.pack_forget()
+        self.modify_frame.pack_forget()
+        self.insert_frame.pack_forget()
+        self.search_frame.pack_forget()
+        self.delete_frame.pack_forget()
+        self.sort_frame.pack_forget()
+        self.total_frame.pack_forget()
+        self.content_frame.pack()
+        pass
+
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    MainPage(master=root)
+    root.mainloop()
---
+++ view.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+import tkinter as tk
+from tkinter import ttk
+
+
+class AboutFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Label(self,text='关于页面').pack()
+
+
+class ModifyFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Label(self,text='修改页面').pack()
+
+class InsertFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        # tk.Label(self,text='录入页面').pack()
+        self.id = tk.StringVar()
+        self.name = tk.StringVar()
+        self.chinese = tk.StringVar()
+        self.math = tk.StringVar()
+        self.english = tk.StringVar()
+        self.insert_data()
+
+
+    def insert_data(self):
+
+        tk.Label(self,text='id').grid(row=1,column=1,pady=10)
+        tk.Entry(self,textvariable=self.id).grid(row=1,column=2,pady=10)
+        tk.Label(self, text='姓名').grid(row=2, column=1, pady=10)
+        tk.Entry(self, textvariable=self.name).grid(row=2, column=2, pady=10)
+        tk.Label(self, text='语文').grid(row=3, column=1, pady=10)
+        tk.Entry(self, textvariable=self.chinese).grid(row=3, column=2, pady=10)
+        tk.Label(self, text='数学').grid(row=4, column=1, pady=10)
+        tk.Entry(self, textvariable=self.math).grid(row=4, column=2, pady=10)
+        tk.Label(self, text='英语').grid(row=5, column=1, pady=10)
+        tk.Entry(self, textvariable=self.english).grid(row=5, column=2, pady=10)
+        tk.Button(self,text='录入',command=self.save_data).grid(row=6,column=2,pady=10)
+
+
+
+
+    def save_data(self):
+        self.student_list = []
+        self.student = {'id': self.id.get(), '姓名': self.name.get(), '语文': self.chinese.get(), '数学': self.math.get(),
+                        '英语': self.english.get()}
+        self.student_list.append(self.student)
+        tk.Label(self, text='录入成功').grid(row=7, column=2)
+        try:
+            stu_txt = open('student.txt','a',encoding='UTF-8')
+        except:
+            stu_txt = open('student.txt','w',encoding='UTF-8')
+        for item in self.student_list:
+            stu_txt.write(str(item) + '\n')
+        stu_txt.close()
+        self.id.set('')
+        self.name.set('')
+        self.chinese.set('')
+        self.math.set('')
+        self.english.set('')
+
+
+
+
+class SearchFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Button(self,text='按照id查询',command=self.id_search).pack()
+        tk.Button(self,text='按照姓名查询').pack()
+        self.id = tk.StringVar()
+        self.name = tk.StringVar()
+
+
+
+    def id_search(self):
+        # self.destroy()
+        self.student_query = []
+        tk.Label(self,text='id').pack()
+        tk.Entry(self,textvariable=self.id).pack()
+        with open('student.txt','r',encoding='UTF-8') as id_search_file:
+            student_info = id_search_file.readlines()
+            for item in student_info:
+                info = dict(eval(item))
+                if self.id.get() !='':
+                    if self.id.get() == info['id']:
+                        self.student_query.append(info)
+
+        tk.Button(self,text='查询',command=self.show_student).pack()
+
+
+    def show_student(self):
+        self.table_view = tk.Frame()
+        columns = ("id","name","chinese","math","english")
+        columns_value = ("id","姓名","语文","数学","英语")
+        self.tree_view = ttk.Treeview(self,show='headings',columns=columns)
+        self.tree_view.column('id',width=80,anchor='center')
+        self.tree_view.column('name', width=80, anchor='center')
+        self.tree_view.column('chinese', width=80, anchor='center')
+        self.tree_view.column('math', width=80, anchor='center')
+        self.tree_view.column('english', width=80, anchor='center')
+        self.tree_view.heading('id',text='id')
+        self.tree_view.heading('name', text='姓名')
+        self.tree_view.heading('chinese', text='语文')
+        self.tree_view.heading('math', text='数学')
+        self.tree_view.heading('english', text='英语')
+        self.tree_view.pack(fill=tk.BOTH,expand=True)
+
+        for item in self.student_query:
+            print(self.id.get())
+
+
+
+class DeleteFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Label(self,text='删除页面').pack()
+
+class SortFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Label(self,text='排序页面').pack()
+
+class TotalFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Label(self,text='统计页面').pack()
+
+class ContentFrame(tk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        tk.Label(self,text='概览页面').pack()
